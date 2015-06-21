d0e187411454a8a33f009a5cf5dbbdb2a36a4086
SmellyCat <smellycat2014@foxmail.com>
2015-06-22 03:14:02 +0800

    Update __ini__ add main

--- upload/__init__.py
+++ upload/__init__.py
@@ -3,5 +3,6 @@ from my_logger import debug_logging, online_logging
 from app import app, db
 from models import Users, Upload
 import views
+from main import Uploader, History, Digger
 
 __version__ = '0.1.0'
